efb742d30dc723cd1484579f845366c093834e5f
Mike Singarayar <michaelsingarayar@gmail.com>
2018-11-29 19:32:29 -0700

    It worksgit add -Agit add -A

--- assests/javascript/logic.js
+++ assests/javascript/logic.js
@@ -28,10 +28,103 @@ $(document).ready(function () {
 
 
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAdPz783_kvw7dVI5FdJAEWb4fQnt-zz18",
+    authDomain: "trail-mis.firebaseapp.com",
+    databaseURL: "https://trail-mis.firebaseio.com",
+    projectId: "trail-mis",
+    storageBucket: "trail-mis.appspot.com",
+    messagingSenderId: "549578174563"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+
+
+
+
+
+  //Initailze Reddit API
+
+  var redditJSON = "https://old.reddit.com/r/coloradohikers.json";
+
+  $.ajax({
+    url: redditJSON,
+    method: "GET"
+  }).then(function (response) {
+
+    console.log(response);
+    console.log(response.data.children[0].data);
+
+
+    for (var i = 0; i < response.data.children.length; i++) {
+
+      console.log(response.data.children[i].data.title);
+      // console.log(response.data.children[i].data.selftext);
+      console.log(response.data.children[i].data.author);
+      console.log(response.data.children[i].data.num_comments);
+      console.log(response.data.children[i].data.url);
+      console.log(response.data.children[i].data.subreddit);
+      console.log(response.data.children[i].data.score);
+
+
+
+
+      var tRow = $("<tr>");
+
+      // Merging the URL for the post with the title of the post.
+      var postTitle = (response.data.children[i].data.title);
+      var postURL = (response.data.children[i].data.url);
+
+      var postLink = '<td><a href="' + postURL + '">"' + postTitle + '"</a></td>';
+
+      // Merging the URL for the comments to a dedicated "comments" link
+
+      var commentsURL = "https://www.reddit.com" + (response.data.children[i].data.permalink);
+
+      var numComments = $("<td>").text(response.data.children[i].data.num_comments);
+      var commentSect = '<td><a href="' + commentsURL + '">Comments</a></td>';
+
+      // var authorName = $("<td>").text(response.data.children[i].data.author);
+      // var subredditName = $("<td>").text(response.data.children[i].data.subreddit);
+
+      var postScore = $("<td>").text(response.data.children[i].data.score);
+
+      tRow.append(postScore, postLink, numComments, commentSect);
+
+      $(".reddit").append(tRow);
+
+
+    }
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   //on click event  that grabs  user inputs and gets lat and lng from geocode api
   $("#submit-btn").on("click", function (event) {
     event.preventDefault();
     var userInput = $(".input-field").val().trim();
+
+
+
+
+
     console.log(userInput);
     //Initalize GeoCode API
     var geoJSON = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userInput + "&key=AIzaSyCW64s7vGbdV-D23-YVGWcTOIeNlMxHpzY"
@@ -57,6 +150,7 @@ $(document).ready(function () {
       console.log("Latitiude " + lat);
       console.log("Longitiude " + lng);
 
+      // Initialize Hiking Api
       var queryURL = "https://www.hikingproject.com/data/get-trails?lat=" + lat + "&lon=" + lng + "&maxDistance=10&maxResults=10&key=200388220-d5e3b13778e51ac99d93ea01e7508197";
 
 
@@ -67,23 +161,43 @@ $(document).ready(function () {
 
         console.log(response);
 
-
+        // for loop that grabs the 10 responses
         for (var j = 0; j < response.trails.length; j++) {
+          //Defines objects for firebase database
+          var na = response.trails[j].name;
+          var sum = response.trails[j].summary;
+          var location = response.trails[j].location;
+          var length = response.trails[j].length;
+          var diff = response.trails[j].difficulty;
+          var con = response.trails[j].conditionStatus;
+          var deets = response.trails[j].conditionDetails;
+
+
+          var userIn = {
+            userInput: userInput,
+            name: na,
+            latitiude: lat,
+            longitiude: lng,
+            summary: sum,
+            location: location,
+            length: length,
+            difficulty: diff,
+            conditionStatus: con,
+            conditionDetails: deets,
+
+          };
+
+          database.ref().push(userIn);
+
+
 
 
           //puts image and name into html
           var divBody = $("<div>");
-          //var link = $("<a href='../Group-Project-1/results.html'>");
+
           var image = $("<img>");
           image.attr("src", response.trails[j].imgMedium);
           console.log("image" + response.trails[j].imgMedium);
-
-
-
-
-
-
-
 
           var name = $("<p>").text(response.trails[j].name);
           name.addClass("align-center");
@@ -93,17 +207,21 @@ $(document).ready(function () {
           $("#first").prepend(divBody);
 
 
+
+          // for loop that makes the images into links to result page
           var images = document.getElementsByTagName("img");
           for (var k = 0; k < images.length; k++) {
             var imageI = images[k];
             imageI.onclick = function (event) {
               window.location.href = this.id + '../Group-Project-1/results.html';
-               var im = $("<img>");
-               im.attr("src",images[k]);
+              var im = $("<img>");
 
-               //need help looking within this new document
+              //thi isnt adding the source of the image
+              im.attr("src", images[k]);
 
-               this.$("#img").append(im);
+              //need help looking within this new document
+
+              this.$("#img").append(im);
 
 
 
@@ -161,7 +279,7 @@ $(document).ready(function () {
 
 
 
-
+  // clears user input feild 
   userInput = $(".input-field").val("");
 
 
@@ -184,73 +302,9 @@ $(document).ready(function () {
 
 
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAdPz783_kvw7dVI5FdJAEWb4fQnt-zz18",
-    authDomain: "trail-mis.firebaseapp.com",
-    databaseURL: "https://trail-mis.firebaseio.com",
-    projectId: "trail-mis",
-    storageBucket: "trail-mis.appspot.com",
-    messagingSenderId: "549578174563"
-  };
-  firebase.initializeApp(config);
 
 
 
-
-  //Initailze Reddit API
-
-  var redditJSON = "https://old.reddit.com/r/coloradohikers.json";
-
-  $.ajax({
-    url: redditJSON,
-    method: "GET"
-  }).then(function (response) {
-
-    console.log(response);
-    console.log(response.data.children[0].data);
-
-
-    for (var i = 0; i < response.data.children.length; i++) {
-
-      console.log(response.data.children[i].data.title);
-      // console.log(response.data.children[i].data.selftext);
-      console.log(response.data.children[i].data.author);
-      console.log(response.data.children[i].data.num_comments);
-      console.log(response.data.children[i].data.url);
-      console.log(response.data.children[i].data.subreddit);
-      console.log(response.data.children[i].data.score);
-
-
-      var tRow = $("<tr>");
-
-      // Merging the URL for the post with the title of the post.
-      var postTitle = (response.data.children[i].data.title);
-      var postURL = (response.data.children[i].data.url);
-
-      var postLink = '<td><a href="' + postURL + '">"' + postTitle + '"</a></td>';
-
-      // Merging the URL for the comments to a dedicated "comments" link
-
-      var commentsURL = "https://www.reddit.com" + (response.data.children[i].data.permalink);
-
-      var numComments = $("<td>").text(response.data.children[i].data.num_comments);
-      var commentSect = '<td><a href="' + commentsURL + '">Comments</a></td>';
-
-      // var authorName = $("<td>").text(response.data.children[i].data.author);
-      // var subredditName = $("<td>").text(response.data.children[i].data.subreddit);
-
-      var postScore = $("<td>").text(response.data.children[i].data.score);
-
-      tRow.append(postScore, postLink, numComments, commentSect);
-
-      $(".reddit").append(tRow);
-
-
-    }
-
-
-  });
 
 
 
